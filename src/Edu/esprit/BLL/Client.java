2ebf1a1324158169107cf641bc7fce936125140f
Elyes <Touch media@Elyes-PC>
2014-02-20 21:32:51 +0100

    Mise à jour Squellet GoldenCage

--- src/Edu/esprit/BLL/Client.java
+++ src/Edu/esprit/BLL/Client.java
@@ -6,10 +6,42 @@
 
 package Edu.esprit.BLL;
 
+import java.sql.Date;
+
 /**
  *
- * @author Touch media
+ * @author Elyes
  */
-public class Client {
+public class Client extends User{
+    private Date dateInscrit;
+
+    public Date getDateInscrit() {
+        return dateInscrit;
+    }
+
+    public void setDateInscrit(Date dateInscrit) {
+        this.dateInscrit = dateInscrit;
+    }
+
+    public Client(Date dateInscrit, String login, String password, String nom, String prenom, String adresse, String email) {
+        super(login, password, nom, prenom, adresse, email);
+        this.dateInscrit = dateInscrit;
+    }
+
+    public Client(Date dateInscrit, String login, String password, String email) {
+        super(login, password, email);
+        this.dateInscrit = dateInscrit;
+    }
+
+    public Client() {
+    }
+
+    @Override
+    public String toString() {
+        return "Info Client :\nLogin : "+getLogin()+"\nPassword : "+getPassword()+"\nE-mail : "+getEmail()
+                +"\n\nInfo Personnel :\nNom & Prenom "+getNom()+" "+getPrenom()+"\nAdresse : "+getAdresse()
+                +"\nDate d'Inscription : "+getDateInscrit();
+    }
+    
     
 }
--- src/Edu/esprit/BLL/User.java
+++ src/Edu/esprit/BLL/User.java
@@ -8,8 +8,87 @@ package Edu.esprit.BLL;
 
 /**
  *
- * @author Touch media
+ * @author Elyes
  */
 public class User {
+    private String login;
+    private String password;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String email;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String login, String password, String nom, String prenom, String adresse, String email) {
+        this.login = login;
+        this.password = password;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.email = email;
+    }
+    
+    public User(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+    
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "Info User :\nLogin : "+getLogin()+"\nPassword : "+getPassword()+"\nE-mail : "+getEmail()
+                +"\n\nInfo Personnel :\nNom & Prenom "+getNom()+" "+getPrenom()+"\nAdresse : "+getAdresse();
+    }
+    
     
 }
